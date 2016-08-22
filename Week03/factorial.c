a51635464380c3d230047159b54fb77f26ee90f0
algot <algot@ua.fm>
2016-08-22 11:00:47 -0400

    add factorial

---
+++ Week03/factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // factorials aren't defined for the negative integers
+    int num;
+    do
+    {
+    	printf("Enter a positive integer: ");
+    	num = GetInt();
+    }
+    while (num < 0);
+
+    int factorial = 1;
+    for (int i = 1; i <= num; i++)
+    {
+    	factorial = factorial * i;
+    }
+    
+    printf("%d! = %d\n", num, factorial);
+
+    return 0;
+}
